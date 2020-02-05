b0aea8d9ddcae7564d58e82a5e683d181e64bf2c
Grzegorz Jeremenko <grzesiula002@gmail.com>
2020-02-05 20:24:49 +0100

    repair of the page change script

--- js/main.js
+++ js/main.js
@@ -1,34 +1,54 @@
 let page = 0;
 let pageMax = 2;
 let pageMin = 0;
+let pageLoad = false;
+let arrowAnimationTimeout;
 
 function nextPage() {
-    if(page < pageMax) page++;
-    else if(page == pageMax) page = pageMin;
- 
-    changePage();
+    if(pageLoad == true) {
+        if(page < pageMax) page++;
+        else if(page == pageMax) page = pageMin;
+    
+        changePage();
+        arrowAnimation();
+    }
 }
 
 function prevPage() {
-    if(page > pageMin) page--;
-    else if(page == 0) page = pageMax;
+    if(pageLoad == true) {
+        if(page > pageMin) page--;
+        else if(page == 0) page = pageMax;
 
-    changePage();
+        changePage();
+        arrowAnimation();
+    }
+}
+
+function arrowAnimation() {
+    document.getElementById("arrowLeft").style.animation = "none";
+    document.getElementById("arrowRight").style.animation = "none";
+
+    clearTimeout(arrowAnimationTimeout);
+    arrowAnimationTimeout = setTimeout(arrowAnimationStart, 5000);
 }
 
 function changePage() {
     if((page < pageMin) || (page > pageMax)) page = pageMin;
 
-    if(page != 0) {
-        document.getElementById("arrowLeft").style.animation = "none";
-        document.getElementById("arrowRight").style.animation = "none";
-    }
+    pageLoad = false;
 
     $( "#page" ).fadeOut( 100, function() {
         $( "#page" ).load( "../page/"+page+".html", function (){
-            $( "#page" ).fadeIn(100);
+            $( "#page" ).fadeIn(100, function () {
+                pageLoad = true;
+            });
         });
     });
+}
+
+function arrowAnimationStart() {
+    document.getElementById("arrowLeft").style.animation = "arrowLeft 1s infinite";
+    document.getElementById("arrowRight").style.animation = "arrowRight 1s infinite";
 }
 
 function logkey(e) {
